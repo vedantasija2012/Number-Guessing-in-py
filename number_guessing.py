76283bf671121e57cf30e25686927c1f61ce2cee
Vedant <vedant.asija@gmail.com>
2022-01-07 13:26:41 +0530

    Guessing game commit for py file

---
+++ number_guessing.py
@@ -0,0 +1,25 @@
+import random
+RandNo=random.randint(1, 100)
+
+user_guess=None
+guesses=0
+
+while user_guess!=RandNo:
+    user_guess=int(input("Enter your Guess: "))
+    guesses+=1
+    if user_guess==RandNo:
+        print("You Guessed the Number Right :)")
+    elif user_guess<RandNo:
+        print("Higher Number please!")
+    else:
+        print("Lower Number please!")
+print(f"You Guessed the Number in {guesses} Guesses")
+
+with open('NumberGuessing_in_py\highScore.txt', 'r') as f:
+    highscore=int(f.read())
+
+if guesses<highscore:
+    print("Congratulations! You made a new High Score :)")
+    with open('NumberGuessing_in_py\highScore.txt', 'w') as f:
+        f.write(str(guesses))
+# C:\Users\vedan\GitHub Projects\NumberGuessing_in_py\highScore.txt
